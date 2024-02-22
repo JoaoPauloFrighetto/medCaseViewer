b57e9c8fb48f9ab42944b5efca10328d7ce2fcd9
Arcovini <160910391+Arcovini@users.noreply.github.com>
2024-02-22 14:30:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Sketchfab Viewer API: Start/Stop the viewer
 var version = "1.9.0";
-var uid = "784e95f4f22545199be7e165af6437f8";
+var uid = "cfd2dea53c7248489c018237768ef943";
 
 var urlParams = new URLSearchParams(window.location.search);
 var autoSpin = 0.0;
